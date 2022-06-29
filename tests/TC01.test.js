7fb35d158797508c7477758c1421a00f222c94d8
SoniaMorones <soniamorones8@gmail.com>
2022-06-29 11:57:55 -0500

    Con el for, funciona y guarda :)

--- tests/TC01.test.js
+++ tests/TC01.test.js
@@ -1,34 +1,40 @@
 const {test, expect} = require('@playwright/test');
 
-//const correo_parte1 = ["sonia"];
-//const correo_parte2= ['@chilliapps.com'];
-//var contador= 0;
+//Para la construcción del correo
+const correo_parte1 = ["sonia"];
+const correo_parte2= ['@chilliapps.com'];
+var contador= 0;
+//Para la construcción del nombre
+const nombre_parte1 = ["Sonia"];
 
-//const nombre_parte1 = ["sonia"];
- 
+//Para la construcción del Primer Apellido
+const Apepat_parte1 = ["ApePat"];
 
 
-    test('Correo1', async ({page})  => {
+    test('Generar una compra en Hardtek', async ({page})  => {
      
-  //    for (var a=0; a==5; a++) {
-    //        contador = contador + 15;
-      //      correoFinal = correo_parte1+contador+correo_parte2; 
-        //    nombreFinal = nombre_parte1+contador; 
-
+    for (var a=0; a<5; a++) {
+         contador = contador + 18;
+           correoFinal = correo_parte1+contador+correo_parte2; 
+           nombreFinal = nombre_parte1+contador; 
+        console.log ("VariableS: ", correoFinal, nombreFinal)
+        console.log ("Contador: ", contador)
+        test.setTimeout(120000000); 
+        
         await page.goto('https://hardtek.myshopify.com/collections/speakers/products/5-satellite-speakers-system')
         await page.locator('button:has-text("Add to Cart")').click();
        // test.setTimeout(80000);
-        //await page.locator('button:has-text("Add to Cart")').nth(1).click();
-      //  test.setTimeout(80000);
-        //await page.locator('text=Proceed to checkout').first().click();
-        await page.locator('button:has-text("Checkout")').click();
+        await page.locator('button:has-text("Add to Cart")').nth(1).click();
+       test.setTimeout(80000);
+        await page.locator('text=Proceed to checkout').first().click();
+     //   await page.locator('button:has-text("Checkout")').click();
 
         /************ AGREGAR DATOS*******************************/
      //   await expect(page.locator('logo__text heading-1')).toHaveText('hardjjtek');
-        await page.locator('[placeholder="Email or mobile phone number"]').fill('sonia77@chilliapps.com');
-  
+        await page.locator('[placeholder="Email or mobile phone number"]').fill(correoFinal);
+        test.setTimeout(80000);
         await page.locator('text=Email me with news and offers We’ll send you an order receipt and recurring ship >> input[type="checkbox"]').check();
-        await page.locator('[placeholder="First name \\(optional\\)"]').fill('nombreFinal');
+        await page.locator('[placeholder="First name \\(optional\\)"]').fill(nombreFinal);
      
         await page.locator('[placeholder="Last name"]').fill('Last Nombre');
         await page.locator('[placeholder="Street and house number"]').fill('Calle ');
@@ -45,8 +51,13 @@ const {test, expect} = require('@playwright/test');
         await page.locator('#checkout_payment_gateway_66737995887').check();  //Clic en pagar con deposito
         test.setTimeout(80000);
         await page.locator('button:has-text("Complete order")').first().click(); // Clic en terminar compra
-        test.setTimeout(1200000);            
-    // } 
+        test.setTimeout(120000000);     
+        test.setTimeout(120000000); 
+        test.setTimeout(8000000); 
+       // await expect(page).toContain('https://hardtek.myshopify.com/13626477/checkouts/4995a4a11ae74ab1cd18a8e6fa83e2c9/thank_you'); 
+       // await expect(page.locator('/html/body/div[1]/div/div/main/div[1]/div[1]/div[1]/div/svg'));
+       await expect.soft(page.locator('btn__content')).toHaveText('Continue shopping');
+      } 
     
   });
 
